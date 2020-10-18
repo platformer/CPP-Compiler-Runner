ed77d636b49bc2c2e68babd9fb40cec9ee0d5b14
platformer <andrewsenth@gmail.com>
2020-10-18 00:29:10 -0500

    added params, added regex for matching file with main function

--- compileCPPWorkspace.cmd
+++ compileCPPWorkspace.cmd
@@ -1,5 +1,6 @@
 echo off & setlocal
 set workspaceDir=%1
 set searchOpt=%2
+set recur=%3
 set batchPath=%~dp0
-powershell.exe -file "%batchPath%findAndCompCPPFiles.ps1" -workDir %workspaceDir%
+powershell.exe -file "%batchPath%findAndCompCPPFiles.ps1" -workDir %workspaceDir% -searchOpt %searchOpt% -recur %recur%
--- findAndCompCPPFiles.ps1
+++ findAndCompCPPFiles.ps1
@@ -6,7 +6,7 @@ otherFiles=""
 Get-ChildItem $workDir -Filter "*.cpp" |
 Where-Object { $_.Attributes -ne "Directory"} |
 ForEach-Object {
-If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern $Text) {
+If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern '(?<!//)[.\r\n]*main\s*\(.*\)[.\r\n]+(?!(?<!/\*)[.\r\n]*\*/') {
 $mainFile=$_.Name
 }
 else{
