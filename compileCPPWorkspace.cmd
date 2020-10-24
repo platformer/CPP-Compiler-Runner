458665594c388f7ace3d650223811883633706d2
platformer <andrewsenth@gmail.com>
2020-10-24 17:42:25 -0500

    search and compile all cpp files (untested)

--- compileCPPWorkspace.cmd
+++ compileCPPWorkspace.cmd
@@ -3,4 +3,10 @@ set workspaceDir=%1
 set searchOpt=%2
 set recur=%3
 set batchPath=%~dp0
-powershell.exe -file "%batchPath%findAndCompCPPFiles.ps1" -workDir %workspaceDir% -searchOpt %searchOpt% -recur %recur%
+set "i=0"
+for /f "delims=" %%a in ('powershell.exe -file %batchPath%\findAndCompCPPFiles.ps1 -workDir %workspaceDir% -searchOpt %searchOpt% -recur %recur%') do(
+	set /A i+=1
+	set "p!i!=%%a"
+)
+echo on
+if %p1% != "" (cd %workspaceDir% && cl %p1% %p2% && echo. && echo. && %p3%) else (echo Could not find main() in workspace)
--- findAndCompCPPFiles.ps1
+++ findAndCompCPPFiles.ps1
@@ -1,15 +1,18 @@
 param([string]$workDir, [string]$searchOpt='none', [bool]$recur=0)
 
-mainFile=""
-otherFiles=""
+$mainFile=""
+$otherFiles=""
+$executable=""
 
 Get-ChildItem $workDir -Filter "*.cpp" |
 Where-Object { $_.Attributes -ne "Directory"} |
 ForEach-Object {
 If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern '(?<!//)[.\r\n]*main\s*\(.*\)[.\r\n]+(?!(?<!/\*)[.\r\n]*\*/') {
 $mainFile=$_.Name
+$executable=$_.BaseName+".exe"
 }
 else{
 $otherFiles+=$_.Name+' '
 }
 }
+return $mainFile,$otherFiles,$executable
