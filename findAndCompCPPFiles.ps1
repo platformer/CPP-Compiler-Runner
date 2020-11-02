554f1ddeba0ab8f1fffc71b59642008d3a358a8a
platformer <andrewsenth@gmail.com>
2020-11-01 19:20:55 -0600

    Get-InCommentRegex function

--- findAndCompCPPFiles.ps1
+++ findAndCompCPPFiles.ps1
@@ -4,15 +4,26 @@ $mainFile=""
 $otherFiles=""
 $executable=""
 
-Get-ChildItem $workDir -Filter "*.cpp" |
-Where-Object { $_.Attributes -ne "Directory"} |
-ForEach-Object {
-If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern '(?<!//)[.\r\n]*main\s*\(.*\)[.\r\n]+(?!(?<!/\*)[.\r\n]*\*/') {
-$mainFile=$_.Name
-$executable=$_.BaseName+".exe"
-}
-else{
-$otherFiles+=$_.Name+' '
-}
+if ($searchOpt -eq 'none') {
+    Get-ChildItem $workDir -Filter "*.cpp" |
+    Where-Object { $_.Attributes -ne "Directory"} |
+    ForEach-Object {
+        If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern '(?<!//).*main[\s\r\n]*\(.*\)[\s\r\n]*\{[.\r\n]*(?!(?<!/\*)[.\r\n]*\*/)') {
+            $mainFile=$_.Name
+            $executable=$_.BaseName+".exe"
+        }
+        else {
+            $otherFiles+=$_.Name+' '
+        }
+    }
 }
 return $mainFile,$otherFiles,$executable
+
+
+function Get-InCommentRegex {
+    param {
+        [string]$searchRegex
+    }
+
+    return '(?<!//).*'+$searchRegex+'[.\r\n]*(?!(?<!/\*)[.\r\n]*\*/)'
+}
