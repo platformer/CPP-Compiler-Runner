5d03ecdc7e6e7f7d460686ab7f30dd8bf780ca68
platformer <andrewsenth@gmail.com>
2020-11-02 19:39:44 -0600

    updated Get-InCommentRegex

--- findAndCompCPPFiles.ps1
+++ findAndCompCPPFiles.ps1
@@ -3,27 +3,40 @@ param([string]$workDir, [string]$searchOpt='none', [bool]$recur=0)
 $mainFile=""
 $otherFiles=""
 $executable=""
+$foundMain=$false
 
 if ($searchOpt -eq 'none') {
     Get-ChildItem $workDir -Filter "*.cpp" |
     Where-Object { $_.Attributes -ne "Directory"} |
     ForEach-Object {
-        If (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern '(?<!//).*main[\s\r\n]*\(.*\)[\s\r\n]*\{[.\r\n]*(?!(?<!/\*)[.\r\n]*\*/)') {
-            $mainFile=$_.Name
+        if (($foundMain -eq $false) -and (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern (Get-InCommentRegex -searchRegex 'main[\s\r\n]*\([\s\r\n]*\)[\s\r\n]*\{'))) {
+            $mainFile=$_.Basename+$_.Extension
             $executable=$_.BaseName+".exe"
+            $foundMain=$true
         }
         else {
             $otherFiles+=$_.Name+' '
         }
     }
 }
+elseif ($searchOpt -eq 'sameName') {
+    Get-ChildItem $workDir -Filter "*.cpp" |
+    Where-Object { $_.Attributes -ne "Directory"} |
+    ForEach-Object {
+        if (($foundMain -eq $false) -and (Get-Content $_.FullName | Select-String -CaseSensitive -Pattern (Get-InCommentRegex -searchRegex 'main[\s\r\n]*\([\s\r\n]*\)[\s\r\n]*\{'))) {
+            $mainFile=$_.Basename+$_.Extension
+            $executable=$_.BaseName+".exe"
+            $foundMain=$true
+        }
+    }
+
+}
+
 return $mainFile,$otherFiles,$executable
 
 
 function Get-InCommentRegex {
-    param {
-        [string]$searchRegex
-    }
+    param ([string]$searchRegex)
 
     return '(?<!//).*'+$searchRegex+'[.\r\n]*(?!(?<!/\*)[.\r\n]*\*/)'
 }
